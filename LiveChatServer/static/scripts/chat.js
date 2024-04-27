0d0ee8678118143a4019f03b03ff09b8478632d9
IsaiahSama <zeldaplayergl@gmail.com>
2024-04-27 13:17:29 -0400

    Added colors! Chooseable Colors!

--- LiveChatServer/static/scripts/chat.js
+++ LiveChatServer/static/scripts/chat.js
@@ -2,6 +2,7 @@ const textareaDiv = document.getElementById('textareas');
 
 const chatName = localStorage.getItem('livechat-name');
 const rcode = localStorage.getItem('livechat-rcode');
+const color = localStorage.getItem('livechat-color');
 
 let socket = null
 
@@ -9,7 +10,7 @@ const setupSockets = () => {
     socket = io();
 
     socket.on('connect', () => {
-        socket.emit('joinRoom', chatName, rcode);
+        socket.emit('joinRoom', chatName, rcode, color);
     })
 
     socket.on('joinedRoom', (data) => {
@@ -28,7 +29,7 @@ const setupSockets = () => {
     })
     
     socket.on('updateClientMembers', (members) => {
-        document.getElementById('members').innerHTML = members.join(', ');
+        document.getElementById('members').innerHTML = Object.keys(members).join(', ');
     })
 
     socket.on('memberJoined', (member) => {
@@ -51,26 +52,26 @@ const setupSockets = () => {
 
 const prepareRooms = (members) => {
     for (let member of members) {
-        prepareRoom(member)
+        prepareRoom(member[0], member[1])
     }
 }
 
 const createTextContainer = () => {
     const textContainer = document.createElement('div');
-    textContainer.className = "flex flex-direction-column align-items-left ceell";
-
-    textContainer.style.borderColor = getRandomColor();
+    textContainer.className = "is-flex is-flex-direction-column cell";
 
     return textContainer;
 }
 
-const createTextarea = (username) => {
+const createTextarea = (username, color) => {
     const textarea = document.createElement('textarea');
     textarea.classList.add('textarea')
     textarea.rows = 10
     textarea.cols = 30
 
     textarea.id = username
+
+    textarea.style.borderColor = color
 
     textarea.addEventListener('input', () => {
         socket.emit('updateServerText', rcode, textarea.id, textarea.value)
@@ -79,8 +80,10 @@ const createTextarea = (username) => {
     return textarea;
 }
 
-const createSpan = (username, color) => {
+const createHeader = (username, color) => {
     const divContainer = document.createElement('div');
+    divContainer.className = "is-flex is-justify-content-space-between is-fullwidth";
+
     const span = document.createElement('span');
     span.innerHTML = `${username}'s Chat `;
     span.style.color = color;
@@ -88,33 +91,28 @@ const createSpan = (username, color) => {
     const button = document.createElement('button');
     button.innerText = "CLEAR";
 
-    button.className = 'button is-primary'
+    button.className = 'button'
+    button.style.backgroundColor = color
 
     button.addEventListener('click', () => {
         document.getElementById(username).value = ""
     })
 
-    return span;
+    divContainer.appendChild(span);
+    divContainer.appendChild(button);
+
+    return divContainer;
 }
 
-const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-const prepareRoom = (username) => {
+const prepareRoom = (username, color) => {
     const textContainer = createTextContainer();
     textContainer.id = username + "Div";
 
-    const span = createSpan(username, getRandomColor());
+    const header = createHeader(username, color);
 
-    const textarea = createTextarea(username);
+    const textarea = createTextarea(username, color);
 
-    textContainer.appendChild(span);
+    textContainer.appendChild(header);
     textContainer.appendChild(textarea);
 
     textareaDiv.appendChild(textContainer);
